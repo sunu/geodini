222845a5f8d72bccbfba12341834b323e36cb042
Tarashish Mishra <sunu@sunu.in>
2025-06-09 18:15:15 +0530

    fix: import weirdness

--- geodini/lib.py
+++ geodini/lib.py
@@ -1,6 +1,7 @@
 from collections.abc import Callable
 from typing import Any
 
+from geodini.agents.utils.geocoder import geocode as overture_divisions_geocode
 from geodini import agents as geodini_agents
 
 
@@ -9,4 +10,4 @@ def get_geocoders(
     geocoders,
 ) -> list[Callable[[str, int | None], list[dict[str, Any]]]]:
     """Get a list of geocoders"""
-    return [geodini_agents.utils.geocoder.geocode]
+    return [overture_divisions_geocode]
