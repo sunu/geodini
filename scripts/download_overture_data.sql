197b0128b0d7f8c3f5fb4b2b966d935340971bc1
Tarashish Mishra <sunu@sunu.in>
2025-06-09 13:05:18 +0530

    dont download land and water data yet

--- scripts/download_overture_data.sql
+++ scripts/download_overture_data.sql
@@ -1,54 +1,29 @@
-LOAD spatial; -- noqa
-LOAD httpfs;  -- noqa
+INSTALL spatial;
+INSTALL httpfs;
+LOAD spatial;
+LOAD httpfs; 
 
 -- Access the data on AWS
 SET s3_region='us-west-2';
 
 SELECT 'Starting Overture data download...' as message;
 
--- -- Download divisions
--- SELECT 'Downloading divisions...' as message;
--- COPY (
---     SELECT *
---     FROM read_parquet('s3://overturemaps-us-west-2/release/2025-02-19.0/theme=divisions/type=division/*')
--- ) TO 'data/divisions.parquet';
-
--- SELECT 'Divisions download complete.' as message;
-
--- -- Download division areas
--- SELECT 'Downloading division areas...' as message;
--- COPY (
---     SELECT *
---     FROM read_parquet('s3://overturemaps-us-west-2/release/2025-02-19.0/theme=divisions/type=division_area/*')
--- ) TO 'data/division_areas.parquet';
-
--- SELECT 'Division areas download complete.' as message;
-
--- Download land data
-SELECT 'Downloading land data...' as message;
+-- Download divisions
+SELECT 'Downloading divisions...' as message;
 COPY (
     SELECT *
-    FROM read_parquet('s3://overturemaps-us-west-2/release/2025-02-19.0/theme=base/type=land/*')
-) TO 'data/land.parquet';
+    FROM read_parquet('s3://overturemaps-us-west-2/release/2025-02-19.0/theme=divisions/type=division/*')
+) TO 'data/divisions.parquet';
 
-SELECT 'Land data download complete.' as message;
+SELECT 'Divisions download complete.' as message;
 
--- Download water data 
-SELECT 'Downloading water data...' as message;
+-- Download division areas
+SELECT 'Downloading division areas...' as message;
 COPY (
     SELECT *
-    FROM read_parquet('s3://overturemaps-us-west-2/release/2025-02-19.0/theme=base/type=water/*')
-) TO 'data/water.parquet';
+    FROM read_parquet('s3://overturemaps-us-west-2/release/2025-02-19.0/theme=divisions/type=division_area/*')
+) TO 'data/division_areas.parquet';
 
-SELECT 'Water data download complete.' as message;
-
--- Download infrastructure data
-SELECT 'Downloading infrastructure data...' as message;
-COPY (
-    SELECT *
-    FROM read_parquet('s3://overturemaps-us-west-2/release/2025-02-19.0/theme=base/type=infrastructure/*')
-) TO 'data/infrastructure.parquet';
-
-SELECT 'Infrastructure data download complete.' as message;
+SELECT 'Division areas download complete.' as message;
 
 SELECT 'All downloads finished successfully!' as message;
